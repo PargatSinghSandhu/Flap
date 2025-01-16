c0542af26328e46677d1b0024fae154a2c3bcbed
PargatSinghSandhu <sandhu.pargat07@gmail.com>
2025-01-16 16:50:02 -0500

    Templates + Layout template

--- flap/app.py
+++ flap/app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template  #render_temlate is a function to render the template
 
 
-app = Flask(__name__)
+app = Flask(__name__) #__name__ special variable that is the name of the module
+#that is like flask can know where to look for files, templates
 
-@app.route('/')
+#this is sort of list of dictonaries to make a temporary database
+posts = [{'author':'Robinn Hood', 'title':'Life of a Human', 'price':'$20'},
+         {'author':'Jason', 'title':'The fin', 'price':'$30'}]
 
-def index():
-    return "Flap Project"
+
+@app.route("/") #route is what we put into our browsers to go into specific page
+@app.route("/home")
+def home():
+    #return ''' html doctype code ''' #this is the genral code we can do, but it will create a mess
+    return render_template('home.html', db = posts) # this posts will give us the access to the posts DB, and can use that
+#posts is a argument which is passed in the home template, and using that we can access the psots DB.
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
 
 
 
